7738fd82d5058849210494795d2b0ca8924558e0
@kavinduushan07 <kavinduushan07@gmail.com>
2025-05-17 13:56:11 +0530

    create PatientService

---
+++ patient-service/src/main/java/com/ushan/patient_sevice/service/PatientService.java
@@ -0,0 +1,26 @@
+package com.ushan.patient_sevice.service;
+
+import com.ushan.patient_sevice.dto.PatientResponseDTO;
+import com.ushan.patient_sevice.mapper.PatientMapper;
+import com.ushan.patient_sevice.model.Patient;
+import com.ushan.patient_sevice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient)).toList();
+    }
+}
