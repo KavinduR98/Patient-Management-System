9eec9deb8a9a25cb12dcb37f36cb50a0329f6157
@kavinduushan07 <kavinduushan07@gmail.com>
2025-05-17 13:30:00 +0530

    create PatientRepo

---
+++ patient-service/src/main/java/com/ushan/patient_sevice/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.ushan.patient_sevice.repository;
+
+import com.ushan.patient_sevice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
