7d07cc8059413a5e741cc62f1508526794405262
@kavinduushan07 <kavinduushan07@gmail.com>
2025-05-17 12:13:27 +0530

    add patient-service int

---
+++ patient-service/src/test/java/com/ushan/patient_sevice/PatientSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ushan.patient_sevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
