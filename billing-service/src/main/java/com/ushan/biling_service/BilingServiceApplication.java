ed2eb05183a4f7724102853d7c38221c09216e23
@kavinduushan07 <kavinduushan07@gmail.com>
2025-05-27 05:34:47 +0530

    init billing service

---
+++ billing-service/src/main/java/com/ushan/biling_service/BilingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ushan.biling_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingServiceApplication.class, args);
+	}
+
+}
---
+++ billing-service/src/test/java/com/ushan/biling_service/BilingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ushan.biling_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
